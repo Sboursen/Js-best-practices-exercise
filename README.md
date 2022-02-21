d47bacf0a43d5ef8a3a5b08ce07dfb12de9e8725
sboursen <dev.sboursen@gmail.com>
2022-02-21 15:06:55 +0100

    Add all files

---
+++ README.md
@@ -0,0 +1,69 @@
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+# TO DO LIST
+
+> **To-do list** is a tool that helps you to organize your day. It simply lists the things that you need to do and allows you to mark them as complete.
+> It was built as an introduction to web dev using ES6 and Webpack
+
+## Built With
+
+- Webpack
+- ![](https://img.shields.io/badge/HTML-5-orange)
+- ![](https://img.shields.io/badge/Tailwind-CSS-blue)
+- ![](https://img.shields.io/badge/JavaScript-ES6-yellow)
+- ![](https://img.shields.io/badge/Webpack-5-green)
+
+## Milestones
+
+- Milestone 1: **list structure**
+- Milestone 2: **improved style**
+- Milestone 3: **crud operation**
+- Milestone 4: **user input validation**
+- Milestone 5: **final style touches**
+- Milestone 6: **secure deploy**
+
+## sneak peek
+
+![](./src/Gif/to-do-list.gif)
+
+## Getting Started
+
+**Prerequisites:** A Web Browser (preferably FireFox or Chrome)
+To get a local copy up and running follow these simple example steps.
+
+### **Option 1**
+
+#### Use the live Website
+
+[Awesome Books Website](https://sboursen.github.io/to-do-tasks/)
+
+### **Option 2**
+
+#### Set up your own copy locally
+
+- Clone [the GitHub Repository](https://github.com/Sboursen/to-do-tasks)
+- Go to the Project `cd to-do-tasks`
+- Run `npm install` to install the _dependencies_
+  \_ Run `npm start` to run the live server.
+
+## About me
+
+👤 **Soufiane Boursen**
+
+- GitHub: [@Sboursen](https://github.com/Sboursen)
+- Twitter: [@sboursen_dev](https://twitter.com/sboursen_dev)
+- LinkedIn: [@sboursen](https://linkedin.com/in/sboursen)
+
+## Contributors
+
+Contributions, issues, and feature requests are welcome!
+
+Feel free to check the [issues page](../../issues/).
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ src/components/task-element-utils.js
@@ -0,0 +1,171 @@
+import * as utils from './utils';
+import { clearButton } from './dom-elements';
+
+export function getCheckedTaskElementId(taskElement) {
+  let res = -1;
+  if (taskElement.children[0].checked === true) {
+    res = Number(taskElement.id.split('-')[1]);
+  }
+  return res;
+}
+
+export function toggleShake(element) {
+  element.classList.toggle('lost-focus-with-errors');
+}
+
+export function validateInputWithColor(element, isValid) {
+  if (isValid) {
+    element.classList.remove('not-valid');
+    element.classList.add('valid');
+  } else {
+    element.classList.remove('valid');
+    element.classList.add('not-valid');
+  }
+}
+
+export function enableClearButton() {
+  clearButton.disabled = false;
+}
+
+function disableClearButton() {
+  clearButton.disabled = true;
+}
+
+function onCheckboxToggle(e) {
+  const checkbox = e.currentTarget;
+  const description = checkbox.parentNode.children[1];
+  if (checkbox.checked) {
+    description.style.textDecoration = 'line-through';
+  } else {
+    description.style.textDecoration = 'none';
+  }
+  enableClearButton();
+}
+
+function addCheckEvent(taskElement) {
+  const checkbox = taskElement.querySelector(
+    'input[type="checkbox"]',
+  );
+  const description = checkbox.parentNode.children[1];
+  if (checkbox.checked) {
+    description.style.textDecoration = 'line-through';
+  } else {
+    description.style.textDecoration = 'none';
+  }
+  checkbox.addEventListener('click', onCheckboxToggle);
+}
+
+function onDescriptionInputFocused(e) {
+  const taskElement = e.target.parentNode.parentNode;
+  const moveButton = taskElement.querySelector(
+    'button.move-button',
+  );
+  const deleteButton = taskElement.querySelector(
+    'button.delete-button',
+  );
+  moveButton.style.zIndex = '-1';
+  deleteButton.style.zIndex = '1';
+  taskElement.style.backgroundColor = '#b99a7d';
+}
+
+function onDescriptionInputBlured(e) {
+  const descriptionInput = e.target;
+  const taskElement = e.target.parentNode.parentNode;
+  const moveButton = taskElement.querySelector(
+    'button.move-button',
+  );
+  const deleteButton = taskElement.querySelector(
+    'button.delete-button',
+  );
+  moveButton.style.zIndex = '1';
+  deleteButton.style.zIndex = '-1';
+  taskElement.style.backgroundColor = 'transparent';
+
+  if (
+    utils.isEmpty(descriptionInput.value)
+    || !utils.isValid(descriptionInput.value)
+  ) {
+    toggleShake(descriptionInput);
+    descriptionInput.focus();
+    disableClearButton();
+  } else {
+    enableClearButton();
+  }
+}
+
+function onDescriptionInputChanged(e) {
+  const descriptionInput = e.target;
+  if (
+    utils.isEmpty(descriptionInput.value)
+    || !utils.isValid(descriptionInput.value)
+  ) {
+    descriptionInput.classList.add('not-valid');
+    descriptionInput.classList.remove('valid');
+    disableClearButton();
+  } else {
+    descriptionInput.classList.remove('not-valid');
+    descriptionInput.classList.add('valid');
+    enableClearButton();
+  }
+}
+
+function addInputEvents(taskElement) {
+  const descriptionInput = taskElement.querySelector(
+    'label.description > input',
+  );
+
+  descriptionInput.addEventListener(
+    'focus',
+    onDescriptionInputFocused,
+  );
+
+  descriptionInput.addEventListener(
+    'blur',
+    onDescriptionInputBlured,
+  );
+
+  descriptionInput.addEventListener(
+    'input',
+    onDescriptionInputChanged,
+  );
+}
+
+export default function createTaskElement(task) {
+  const tmpWrapper = document.createElement('div');
+  const taskStringElement = `<li class="task" id="task-${
+    task.index
+  }">
+        <input type="checkbox" name="check-${
+  task.index
+}" id="check-${task.index}" ${
+  task.completed ? 'checked' : null
+}>
+        <label for="description" class="description">
+          <input type="text" name="description" value="${
+  task.description
+}" id="description-${task.index}">
+        </label>
+        <button type="button" class="icon move-button" id="moveButton-${
+  task.index
+}">
+          <i class="fa-solid fa-ellipsis-vertical"></i>
+        </button>
+        <button type="button" class="icon delete-button" id="deleteButton-${
+  task.index
+}">
+          <i class="fas fa-trash-alt"></i>
+        </button>
+      </li>`;
+  tmpWrapper.innerHTML = taskStringElement.trim();
+
+  const taskElement = tmpWrapper.firstElementChild;
+  addCheckEvent(taskElement);
+  addInputEvents(taskElement);
+  return taskElement;
+}
+
+export function allocateSpaceForToDOList(toDoList) {
+  toDoList.style.gridTemplateRows = `repeat(${
+    Object.keys(toDoList).length
+  }, 48px);`;
+}
